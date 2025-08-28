2f32f1554ab982387d5c1c57c51aa553af0eb9c2
Meir Livneh <mlivneh@gmail.com>
2025-08-28 12:09:32 +0300

    Resume tracking firebase-config.js

---
+++ public/js/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyByKMGr2knkWL8Ps_4DoA9KOJ8Ta0IjN-I",
+  authDomain: "fireclassstudio.firebaseapp.com",
+  projectId: "fireclassstudio",
+  storageBucket: "fireclassstudio.firebasestorage.app",
+  messagingSenderId: "900608873995",
+  appId: "1:900608873995:web:130533a437b1f168f7190e",
+  measurementId: "G-RN24RDVPJ4"
+};
